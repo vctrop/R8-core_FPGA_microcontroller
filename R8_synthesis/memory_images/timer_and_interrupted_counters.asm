292a21eb13963d79122ea7286bbd87c395f61c37
Victor O. Costa <victor-o.c@hotmail.com>
2019-04-30 15:41:47 -0300

    Debug variable initialization and implement refresh_timers subroutine

--- R8_synthesis/memory_images/timer_and_interrupted_counters.asm
+++ R8_synthesis/memory_images/timer_and_interrupted_counters.asm
@@ -13,34 +13,34 @@ boot:
     ;reset variables:
 	ldh r8, #display
 	ldl r8, #display
-	st  r0, r8, r5 				;display[0] <- 0
+	st  r0, r8, r5 				; display[0] <- 0
 	addi r5, #1
-	st  r0, r8, r5 				;display[1] <- 0
+	st  r0, r8, r5 				; display[1] <- 0
 	addi r5, #1
-	st  r0, r8, r5 				;display[2] <- 0
+	st  r0, r8, r5 				; display[2] <- 0
 	addi r5, #1
-	st  r0, r8, r5				;display[3] <- 0
+	st  r0, r8, r5				; display[3] <- 0
 	
 	ldh r8, #display_index
 	ldl r8, #display_index
-	st  r0, r8, r0				;display_index <- 0
+	st  r0, r8, r0				; display_index <- 0
 	
 	ldh r8, #debounce_flag
 	ldl r8, #debounce_flag
-	st  r0, r8, r0				;debounce_flag <- 0
+	st  r0, r8, r0				; debounce_flag <- 0
 	
-	ldh r8, #four_ms_counter
-	ldl r8, #four_ms_counter
-	ldh r6, #C5h
-	ldl r6, #30h
-	st  r6, r8, r0			;four_ms_counter <- 50e3
+	ldh r8, #one_ms_counter
+	ldl r8, #one_ms_counter
+	ldh r6, #61h
+	ldl r6, #A8h
+	st  r6, r8, r0			    ; one_ms_counter <- 25e3
 	
 	
-	ldh r8, #four_ms_counter
-	ldl r8, #four_ms_counter
-	ldh r6, #00h
-	ldl r6, #FAh
-	st  r6, r8, r0			;one_s_counter <- 250
+	ldh r8, #one_s_counter
+	ldl r8, #one_s_counter
+	ldh r6, #03h
+	ldl r6, #E8h
+	st  r6, r8, r0			;one_s_counter <- 1000
 	
     ldh r8, #80h            ;
     ldl r8, #01h            ; r8 <= PortA regConfig address
@@ -62,15 +62,12 @@ boot:
     ldl r9, #FCh            ; r9 <= PortA regEnable content
     st r9, r8, r0           ; Write regEnable content on its address
     
-    ;ldh r10, #80h           ;
-    ;ldl r10, #02h           ; r10 <= PortA regData address
-    
     xor r0, r0, r0
     jmpd #main
 
 
 .org #0040h
-interruption_handler:
+interruption_subroutine:
     push r0
     push r5
     push r6
@@ -80,20 +77,20 @@ interruption_handler:
     xor r0, r0, r0
     ldh r5, #80h
     ldl r5, #02h
-    ld r6, r5, r0           ; r6 <- regData
+    ld r6, r5, r0               ; r6 <- regData
     
     ldh r5, #00h
-    ldl r5, #08h            ; r5 <- increment interruption mask
-    and r7, r6, r5          ;
+    ldl r5, #08h                ; r5 <- increment interruption mask
+    and r7, r6, r5              ;
     jmpzd #isr_increment_false
-    jsr #increment_handler
+    jsrd #increment_handler
     isr_increment_false:
     
     ldh r5, #00h
-    ldl r5, #04h            ; r5 <- decrement interruption mask
+    ldl r5, #04h                ; r5 <- decrement interruption mask
     and r7, r6, r5
     jmpzd #isr_decrement_false
-    jsr #decrement_handler
+    jsrd #decrement_handler
     isr_decrement_false:
     
     popf
@@ -102,32 +99,96 @@ interruption_handler:
     pop r5
     pop r0
     rti
-;end interruption_handler
+;end interruption_subroutine
 
-
+; Specific interruption handlers
 increment_handler:
     
+    rts
 
 decrement_handler:
     
+    rts
+; Interruption handling end
 
-update_timer_counters:
 
-
-write_display:
 
 main:
     
+    ; call srt_subtract_cycles
+    ; if r3 == 0
+    ;   refresh displays
+    ;   if r1 == 0
+    ;       update timer
+    
+    ldh r1, #00h
+    ldl r1, #0Ah
+    
+    jsrd #srt_subtract_cycles
+    
     jmpd #main
+
+; Subroutines
+srt_subtract_cycles:
+    ; Objective: to decrement one_ms and one_s counters according to processor usage
+    ; Argument: r1 as number of instructions used
+    ; Return: r3 as one_ms_counter state and r4 as one_s_counter state (1 if still positive)
+    push r0
+    
+    xor r0, r0, r0
+    xor r3, r3, r3          ; ret0 <- 0
+    xor r4, r4, r4          ; ret1 <- 0
+
+    sl0 r5, r1                      ; r5 <- r1*2 (instructions to pseudocycles conversion)
+                                    ; a pseudocycle is equivalent to 2 cycles                      
+    ldh r6, #one_ms_counter    ;
+    ldl r6, #one_ms_counter    ;
+    ld r7, r6, r0                   ; r7 <- mem[one_ms_counter]
+    subi r7, #28                    ; compensate for the 14 instructions of the subroutine
+    sub r7, r7, r5                  ; r7 <- mem[one_ms_counter] - arg*2
+    jmpzd #cyc_zero_or_neg          ;
+    jmpnd #cyc_zero_or_neg          ; if one_ms_counter is positive:
+    addi r3, #1                     ;   ret0 <- 1
+    st r7, r6, r0                   ;   mem[one_ms_counter] <- mem[one_ms_counter] - arg*2
+    jmpd #condition_end     
+    cyc_zero_or_neg:                ; else
+    ldh r7, #61h                    ;
+    ldl r7, #A8h                    ;
+    st r7, r6, r0                   ;   reset one_ms_counter
+        
+    ldh r6, #one_s_counter           ;
+    ldl r6, #one_s_counter           ;
+    ld r7, r6, r0                   ; 
+    subi r7, #1                     ;   r7 <- mem[one_s_counter] - 1
+    jmpzd #mili_zero_or_neg         ;       
+    jmpnd #mili_zero_or_neg         ;   if one_s_counter is positive:
+    addi r4, #1                     ;       ret1 <- 1
+    st r7, r6, r0                   ;       mem[one_s_counter] <- mem[one_s_counter] - 1
+    jmpd #condition_end     
+    mili_zero_or_neg:               ;   else
+    ldh r7, #03h                    ;
+    ldl r7, #E8h                    ;
+    st r7, r6, r0                   ;       reset one_s_counter
+    condition_end:
+    
+    pop r0
+    rts
+
+update_timer_counters:
+    rts
+
+write_display:   
+    rts
+    
 .endcode
 
 
 .data
 	seg_codes:  			db #0300h, #9F00h, #2500h, #0D00h, #9900h, #4900h, #4100h, #1F00h, #0100h, #0900h
-	display: 				db #0000h, #0000h, #0000h, #0000h						;display[0] = units timer, display[3] = tens counter
-	enable_display_mask: 	db #00E0h, #00D0h, #00B0h, #0070h                       ;display enable active at 0
-	display_index: 			db #0000h												;selects witch display is enabled	
+	display: 				db #0000h, #0000h, #0000h, #0000h						; display[0] = units timer, display[3] = tens counter
+	enable_display_mask: 	db #00E0h, #00D0h, #00B0h, #0070h                       ; display enable active at 0
+	display_index: 			db #0000h												; selects witch display is enabled	
 	debounce_flag:			db #0000h												;
-	four_ms_counter:		db #C530h												;50e3 instructions result in 4ms
-	one_s_counter:			db #00FAh												;250 times of 4 ms counter resets results in a second
+	one_ms_counter:		    db #61A8h												; 25e3 instructions result in 4ms
+	one_s_counter:			db #03E8h												; 1000 times of 4 ms counter resets results in a second
 .enddata
