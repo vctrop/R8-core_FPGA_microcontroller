f99e72bfab16d5107342fb363b9d022e4bc33faa
julio <julio.vicenzi@ecomp.ufsm.br>
2019-06-03 20:20:34 -0300

    adds bubblesort with crescent and descrent order

--- R8_synthesis/memory_images/string.asm
+++ R8_synthesis/memory_images/string.asm
@@ -1,6 +1,6 @@
 .code
 boot:
-	ldh r5, #0
+	ldh r5, #0fh
 	ldl r5, #ffh
 	ldsp r5
 	jmpd #main
@@ -159,7 +159,65 @@ integer_to_hexstring:
 	pop r10
 	rts
 ;end integer_to_hexstring
-	
+
+BubbleSort:
+; Objective: sorts vector based on order (0 for crescent and 1 for descrent)
+; Argument: r1 <- &vector, r2 <- size, on stack: order
+; Return: NULL
+    ; Initialization code
+	pop r9		
+	pop r10    				; r10 <- argument 3 (order)
+	push r9
+    xor r0, r0, r0          ; r0 <- 0
+    add r3, r2, r1          ; r3 points the end of array (right after the last element)
+    
+    ldl r4, #0              ;
+    ldh r4, #1              ; r4 <- 1
+    
+    
+; Main code
+scan_bs:
+    addi r4, #0             ; Verifies if there was element swaping
+    jmpzd #end_bs              ; If r4 = 0 then no element swaping
+    
+    xor r4, r4, r4          ; r4 <- 0 before each pass
+    
+    add r5, r1, r0          ; r5 points the first arrar element
+    
+    add r6, r1, r0          ;
+    addi r6, #1             ; r6 points the second array element
+    
+; Read two consecutive elements and compares them    
+loop_bs:
+    ld r7, r5, r0           ; r7 <- array[r5]
+    ld r8, r6, r0           ; r8 <- array[r6]
+	addi r10, #0				; check order
+	jmpzd #crescent_order_bs
+	sub r2, r7, r8
+	jmpnd #swap_bs
+	jmpd #continue_bs
+	crescent_order_bs:
+    sub r2, r8, r7          ; If r8 > r7, negative flag is set
+    jmpnd #swap_bs             ; (if array[r5] > array[r6] jump)
+    
+; Increments the index registers and verifies is the pass is concluded
+continue_bs:
+    addi r5, #1             ; r5++
+    addi r6, #1             ; r6++
+    
+    sub r2, r6, r3          ; Verifies if the end of array was reached (r6 = r3)
+    jmpzd #scan_bs             ; If r6 = r3 jump
+    jmpd #loop_bs              ; else, the next two elements are compared
+
+; Swaps two array elements (memory)
+swap_bs:
+    st r7, r6, r0           ; array[r6] <- r7
+    st r8, r5, r0           ; array[r5] <- r8
+    ldl r4, #1              ; Set the element swaping (r4 <- 1)
+    jmpd #continue_bs
+end_bs:
+    halt
+;end bubblesort
 main:
 	;these must be restored between calls
 	ldh r10, #0
@@ -170,17 +228,29 @@ main:
 	ldl r12, #12
 	
     xor r0, r0, r0
-    ldh r2, #string
-    ldl r2, #string
-	ldh r1, #7fh
-	ldl r1, #ffh
-	jsrd #integer_to_hexstring
-
+    ldh r1, #array
+	ldl r1, #array
+	ldh r2, #size
+	ldl r2, #size
+	ld r2, r2, r0
+	push r0
+	jsrd #bubblesort
+	; xor r0, r0, r0
+    ; ldh r1, #array
+	; ldl r1, #array
+	; ldh r2, #size
+	; ldl r2, #size
+	; ld r2, r2, r0
+	; addi r0, #1
+	; push r0 
+	; jsrd #bubblesort
 	halt
 	
 .endcode
 
-.org #150
+.org #200
 .data
-	string: db #FFFFh #FFFFh #FFFFh #FFFFh #FFFFh #FFFFh #FFFFh
+	; string: db #FFFFh #FFFFh #FFFFh #FFFFh #FFFFh #FFFFh #FFFFh
+	array:  db #50 , #49 , #48 , #47 , #46 , #45 , #44 , #43 , #42 , #41 ,#40 , #39 , #38 , #37 , #36 , #35 , #34 , #33 , #32 , #31 , #30 , #29 , #28 , #27 , #26 , #25 , #24 , #23 , #22 , #21 , #20 , #19 , #18 , #17 , #16 , #15 , #14 , #13 , #12 , #11 , #10 , #9 , #8 , #7 , #6 , #5 , #4 , #3 , #2 , #1 
+	size:	db #50
 .enddata
